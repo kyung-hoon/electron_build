b9bcdff03a707f426f117d75132c9f4e77d2a764
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-06-15 17:16:13 +0900

    중간 커밋

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import fs from 'fs';
+import formidable from 'express-formidable';
+import path from 'path';
 
 const app =express();
 const port =3300;
 
-app.use(express.json());
+app.use(formidable({
+    encoding: 'utf-8',
+    uploadDir: path.join(__dirname, 'target'),
+    multiples: true,
+    keepExtensions:true// req.files to be arrays of files
+    }));
 app.post('/ElectronBuild',(req,res)=>{
     console.log("request received");
+    console.log(req.targetFile);
 });
 
 app.listen(port, ()=>console.log('electron build service is listening'));
