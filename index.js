7cc05234174fd4aee1b769c29b22badbcf2a59d1
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-06-15 17:55:15 +0900

    express-fileupload사용

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import express from 'express';
 import fs from 'fs';
 import formidable from 'express-formidable';
 import path from 'path';
+import fileUpload from 'express-fileupload';
 
 const app =express();
 const port =3300;
 
-app.use(formidable({
-    encoding: 'utf-8',
-    uploadDir: path.join(__dirname, 'target'),
-    multiples: true,
-    keepExtensions:true// req.files to be arrays of files
-    }));
+// app.use(formidable({
+//     encoding: 'utf-8',
+//     uploadDir: path.join(__dirname, 'target'),
+//     multiples: true,
+//     keepExtensions:true// req.files to be arrays of files
+//     }));
+
+app.use(fileUpload());
 app.post('/ElectronBuild',(req,res)=>{
     console.log("request received");
-    console.log(req.targetFile);
+    console.log(req.files);
 });
 
 app.listen(port, ()=>console.log('electron build service is listening'));
