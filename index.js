5765aedd2d48dd8d8c3b77cc157d88ab6b221db9
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-06-16 14:02:19 +0900

    index.js수정

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const port =3300;
 
 app.use(fileUpload());
 app.post('/ElectronBuild',(req,res,next)=>{
-
-    fs.removeSync(path.join(__dirname,"target.zip"));
+    const targetPath = path.join(__dirname,'target');
+    if(fs.existsSync(path.join(__dirname,'target.zip')){
+        fs.removeSync(path.join(__dirname,"target.zip"));
+    }
+    if(fs.existsSync(targetPath)){
+        fs.removeSync(targetPath);
+    }
+    fs.mkdirSync(targetPath);
     let uploadFiles  = req.files.targetFile;
     const fileName = req.files.targetFile.name;
     uploadFiles.mv(path.join(__dirname,fileName));
-    fs.createReadStream(path.join(__dirname,'target.zip')).pipe(unzip.Extract({path: path.join(__dirname)}));
+    fs.createReadStream(path.join(__dirname,'target.zip')).pipe(unzip.Extract({path: targetPath}));
 });
 
 app.listen(port, ()=>console.log('electron build service is listening'));
